589adc80dfd7bc99cb0a72a7745069e3d6c13fec
Zenith Kandel <kandelze123@gmail.com>
2025-11-24 21:00:17 +0545

    feat: streamline CORS handling by including configuration file in login script

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,15 +1,5 @@
 <?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost"); // Must be specific origin for credentials
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Credentials: true");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
+include_once '../config/cors.php';
 session_start();
 include_once '../config/database.php';
 
