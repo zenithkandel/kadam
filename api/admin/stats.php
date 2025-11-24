c5d4ded60700670329cc768ffb34873850bb8472
Zenith Kandel <kandelze123@gmail.com>
2025-11-24 21:04:05 +0545

    feat: centralize CORS handling by including configuration file in multiple scripts

--- api/admin/stats.php
+++ api/admin/stats.php
@@ -1,11 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Credentials: true");
-
+include_once '../config/cors.php';
 include_once '../config/database.php';
 include_once '../utils/auth_check.php';
 
--- api/tasks/accept_application.php
+++ api/tasks/accept_application.php
@@ -1,15 +1,5 @@
 <?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Credentials: true");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
+include_once '../config/cors.php';
 include_once '../config/database.php';
 include_once '../utils/auth_check.php';
 
